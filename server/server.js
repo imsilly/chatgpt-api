565568fb66b77c85bdabb5931873971e89b8b7ba
imsilly <silly.net@163.com>
2023-03-07 16:58:41 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: 'Hello from Bitmvp!'
   })
 })
 
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('守护者AI服务已启动'))
